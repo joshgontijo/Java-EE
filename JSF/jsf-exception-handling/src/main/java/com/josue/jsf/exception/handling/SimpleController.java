9eeadc36fc8dc5528d574d6081d3e61523828338
Josue <josue.eduardo206@gmail.com>
2015-05-16 18:21:27 +0100

    Added jsf-exception-handling

---
+++ JSF/jsf-exception-handling/src/main/java/com/josue/jsf/exception/handling/SimpleController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josue.jsf.exception.handling;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Josue
+ */
+@Named
+@RequestScoped
+public class SimpleController {
+
+    public void checkedException() throws Exception {
+        throw new Exception("SOMETHING WRONG HAPPENED !!! OMG *CHECKED EXCEPTION*");
+    }
+
+    public void uncheckedException() {
+        throw new RuntimeException("SOMETHING WRONG HAPPENED !!! OMG *UNCHECKED EXCEPTION*");
+    }
+
+    public void exceptionMessage() {
+        try {
+            throw new Exception("SOMETHING WRONG HAPPENED !!! OMG *EXCEPTION MESSAGE*");
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+        }
+    }
+
+}
