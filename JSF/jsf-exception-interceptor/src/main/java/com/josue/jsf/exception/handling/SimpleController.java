d5e59c81cef1d6ff76331e52f38140749127fbb8
Josue <josue.eduardo206@gmail.com>
2015-12-01 22:39:37 +0000

    Added jsf-exception-interceptor

---
+++ JSF/jsf-exception-interceptor/src/main/java/com/josue/jsf/exception/handling/SimpleController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.josue.jsf.exception.handling;
+
+import com.josue.jsf.exception.handling.cdi.ExceptionHandled;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Josue
+ */
+@Named
+@RequestScoped
+@ExceptionHandled
+public class SimpleController {
+
+    public void checkedException() throws Exception {
+        throw new Exception("SOMETHING WRONG HAPPENED !!! OMG *CHECKED EXCEPTION*");
+    }
+
+    public void uncheckedException() {
+        throw new RuntimeException("SOMETHING WRONG HAPPENED !!! OMG *UNCHECKED EXCEPTION*");
+    }
+
+    public void exceptionMessage() {
+        try {
+            throw new Exception("SOMETHING WRONG HAPPENED !!! OMG *EXCEPTION MESSAGE*");
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "summary", e.getMessage()));
+        }
+    }
+
+}
