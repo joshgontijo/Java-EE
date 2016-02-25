f85c72b476333ef3baa48fabb4fac41daa008e76
Josue Gontijo <josue.eduardo206@gmail.com>
2016-02-25 21:02:01 +0000

    Fixed testng issue

--- CDI/cdi-synchronized/src/test/java/com/josue/cdi/sync/SampleResourceIT.java
+++ CDI/cdi-synchronized/src/test/java/com/josue/cdi/sync/SampleResourceIT.java
@@ -1,17 +1,16 @@
 package com.josue.cdi.sync;
 
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.testng.Assert;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.inject.Inject;
-import javax.naming.InitialContext;
 import java.util.logging.Logger;
 
 /**
@@ -22,8 +21,8 @@ public class SampleResourceIT extends Arquillian {
     @Inject
     SyncControl control;
 
-    @ArquillianResource
-    protected InitialContext initialContext;
+    @Resource
+    private ManagedExecutorService service;
 
     private static final Logger logger = Logger.getLogger(SampleResourceIT.class.getName());
 
@@ -34,16 +33,26 @@ public class SampleResourceIT extends Arquillian {
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    @BeforeSuite(groups = "arquillian")
-    public void beforeSuite() {
-    }
 
-
-//    @Test(invocationCount = 5, threadPoolSize = 2) // bug: No TestRunnerAdaptor found, @BeforeSuite has not been called
-    @Test(invocationCount = 2)
+    @Test
     public void testSynchronized(){
+
+        TestWorker worker = new TestWorker();
+        service.submit(worker);
+        service.submit(worker);
+        service.submit(worker);
+        service.submit(worker);
+
         int accessId = control.runSynchronized();
         logger.info(":: RESPONSE -> " + accessId + " ::");
         Assert.assertNotSame(accessId, 0);
+    }
+
+    class TestWorker implements Runnable{
+
+        @Override
+        public void run() {
+            control.runSynchronized();
+        }
     }
 }
