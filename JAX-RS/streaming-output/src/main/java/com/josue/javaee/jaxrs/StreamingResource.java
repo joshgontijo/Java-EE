c562989d4d78ab13ede34d11c9bb6af89bc12bbb
Josue Gontijo <josue.eduardo206@gmail.com>
2016-04-23 22:17:01 +0100

    Added upload

--- JAX-RS/streaming-output/src/main/java/com/josue/javaee/jaxrs/StreamingResource.java
+++ JAX-RS/streaming-output/src/main/java/com/josue/javaee/jaxrs/StreamingResource.java
@@ -1,15 +1,21 @@
 package com.josue.javaee.jaxrs;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 /**
  * Created by Josue on 23/04/2016.
@@ -30,6 +36,17 @@ public class StreamingResource {
         StreamingOutput stream = getStreamingOutput();
         return Response.ok(stream).header("Content-Disposition", "attachment; filename=\"sample.txt\"").build();
     }
+
+    @POST
+    @Consumes("*/*") // to accept all input types
+    public Response upload(InputStream stream) throws IOException {
+
+        String fileDestination = System.getProperty("user.home") + UUID.randomUUID().toString().substring(0,4) + ".txt";
+
+        Files.copy(stream, Paths.get(new File(fileDestination).toURI()));
+        return Response.ok().entity("Uploaded to: " + fileDestination).build();
+    }
+
 
     private StreamingOutput getStreamingOutput() {
         return new StreamingOutput() {
